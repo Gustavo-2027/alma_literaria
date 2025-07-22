36d0be6eb027aa6829305aaae5d976c6e5bce26f
Gustavo-2027 <gustavonas.27@gmail.com>
2025-07-22 14:09:24 -0300

    feat: configura roteamento por meio da main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./assets/pages/Login";
-import Private from "./assets/pages/Private";
-import Home from "./assets/pages/Home";
+import Login from "./pages/Login";
+import Private from "./pages/Private";
+import Home from "./pages/Home";
+import BookDetails from "./pages/BookDetails";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const router = createBrowserRouter([
   {
@@ -19,10 +22,20 @@ const router = createBrowserRouter([
       </Private>
     ),
   },
+  {
+    path: "/book/:id",
+    element: (
+      <Private>
+        <BookDetails />
+      </Private>
+    ),
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
